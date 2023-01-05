291930ed4c2859426a854becaf67cfaac1046a6c
sixthou <justyuk@naver.com>
2023-01-05 17:02:57 +0900

    feat : 기본 세팅 변경

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":"테스트 테스트   테스트 포스트 입니다유:  ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/test/",
+        "teaser": null
+      }]
